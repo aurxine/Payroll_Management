aa48e415a30529b7330c0a360086c1f401597b2f
aurxine <sarowardhrubo@gmail.com>
2022-08-05 16:35:10 +0600

    rounding added

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -5,52 +5,41 @@ import datetime
 import itertools
 
 
-# df.drop(df.columns[[0]], axis=1, inplace=True)
-# df.drop(["Total Hour", "Note"], axis=1, inplace=True)
-# print(df.head())
+def roundTime(dt=None, roundTo=60):
+   """Round a datetime object to any time lapse in seconds
+   dt : datetime.datetime object, default now.
+   roundTo : Closest number of seconds to round to, default 1 minute.
+   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+   """
+   if dt == None : dt = datetime.datetime.now()
+   seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+   rounding = (seconds+roundTo/2) // roundTo * roundTo
+   return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
 
+def round_time(dt=None, date_delta=datetime.timedelta(minutes=1), to='average'):
+    """
+    Round a datetime object to a multiple of a timedelta
+    dt : datetime.datetime object, default now.
+    dateDelta : timedelta object, we round to a multiple of this, default 1 minute.
+    from:  http://stackoverflow.com/questions/3463930/how-to-round-the-minute-of-a-datetime-object-python
+    """
+    round_to = date_delta.total_seconds()
+    if dt is None:
+        dt = datetime.now()
+    seconds = (dt - dt.min).seconds
 
-# for i, row in df.iterrows():
-#     if i == 50:
-#         break
-#     if row["In Time"] == "-":
-#         continue
-#     temp_data_dictionary["User ID"] = row["User ID"]
-#     temp_data_dictionary["User Name"] = row["User Name"]
-#     temp_data_dictionary["Date"] = row["Date"]
-#     temp_data_dictionary["Time"] = row["In Time"]
-#     initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])
-    
-#     if row["Out Time"] == "-":
-#         temp_data_dictionary["Time"] = "11:59 PM"
-#         initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])
-#         temp_data_dictionary["Date"] = datetime.datetime.strftime(datetime.datetime.strptime(temp_data_dictionary["Date"], "%Y-%m-%d") \
-#                                                         + datetime.timedelta(days = 1), "%Y-%m-%d")
-#         temp_data_dictionary["Time"] = "12:00 AM"
-#         initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])
-#     else:
-#         temp_data_dictionary["Time"] = row["Out Time"]
-#         initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])
+    if seconds % round_to == 0 and dt.microsecond == 0:
+        rounding = (seconds + round_to / 2) // round_to * round_to
+    else:
+        if to == 'up':
+            # // is a floor division, not a comment on following line (like in javascript):
+            rounding = (seconds + dt.microsecond/1000000 + round_to) // round_to * round_to
+        elif to == 'down':
+            rounding = seconds // round_to * round_to
+        else:
+            rounding = (seconds + round_to / 2) // round_to * round_to
 
-# for i, g in df.groupby(["User ID"]):
-#     for j, row in g.iterrows():
-#         if row["In Time"] == "-":
-#             continue
-#         temp_data_dictionary["User ID"] = row["User ID"]
-#         temp_data_dictionary["User Name"] = row["User Name"]
-#         temp_data_dictionary["Date"] = row["Date"]
-#         temp_data_dictionary["Time"] = row["In Time"]
-#         initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])
-#         if row["Out Time"] == "-":
-#             continue
-#         temp_data_dictionary["Time"] = row["Out Time"]
-#         initally_processed_df = pd.concat([initally_processed_df, pd.DataFrame.from_records([temp_data_dictionary])])        
-        
-# print(initally_processed_df.head())
-# initally_processed_df.to_csv(current_working_directory+data_folder+"processed.csv")
-
-# finally_processed_df = pd.DataFrame(columns= initally_processed_df_columns)
-
+    return dt + datetime.timedelta(0, rounding - seconds, - dt.microsecond)
 
 class ProcessInputData():
     def __init__(self, df:pd.DataFrame=None, drop_columns:list=None) -> None:
@@ -126,19 +115,37 @@ class ProcessInputData():
             self.processed_df.drop(self.processed_df.index[[x[tuple_index] for x in self.problem_rows]], axis=0, inplace=True)
         else:
             print("Index out of range")
-                
+            
+            
+    def roundDateTime(self, nearest_minutes = 20):
+        # self.processed_df[column_name] = roundTime(self.processed_df[column_name], nearest_minutes)
+        # self.processed_df[column_name].mask(self.processed_df[column_name], roundTime(self.processed_df[column_name], nearest_minutes), inplace=True)
+        for i, row in self.processed_df.iterrows():
+            unrounded_date = row["Date"]
+            unrounded_time = row["Time"]
+            unrounded_date_time = datetime.datetime.strptime(unrounded_date + " " + unrounded_time, "%Y-%m-%d %I:%M:%S %p")
+            rounded_date_time = round_time(unrounded_date_time, datetime.timedelta(minutes=nearest_minutes), to="average")
+            rounded_date, rounded_time, p = rounded_date_time.strftime("%Y-%m-%d %I:%M:%S %p").split()
+            rounded_time += " " + p
+            
+            self.processed_df.at[i, "Date"] = rounded_date
+            self.processed_df.at[i, "Time"] = rounded_time
                 
 if __name__=="__main__":
     current_working_directory = os.getcwd()
-    data_folder = "/data_processing/data/"
-    file_name = "Nextzen Attendance Management Portal.xlsx"
+    data_folder = "/data/"
+    file_name = "Nextzen Attendance Management Portal-July.xlsx"
 
     df = pd.read_excel(current_working_directory + data_folder + file_name)
     print(df.head())
     data_processor = ProcessInputData(df=df, drop_columns=["Unnamed: 0", "Total Hour", "Note"])
     data_processor.convertToRawData()
-    # print(data_processor.processed_df.head())
     # print("size before: ",data_processor.processed_df.shape[0])
-    # data_processor.eliminateProblemRows(2)
-    # print("size after: ",data_processor.processed_df.shape[0])
     print(data_processor.problem_df.head())
+    data_processor.eliminateProblemRows(1)
+    # print("size after: ",data_processor.processed_df.shape[0])
+    print(data_processor.processed_df.head())
+    # data_processor.processed_df.to_csv(current_working_directory + data_folder + "july_data.csv")
+    data_processor.roundDateTime()
+    print(data_processor.processed_df.head())
+    # data_processor.processed_df.to_csv(current_working_directory + data_folder + "july_data_rounded.csv")
